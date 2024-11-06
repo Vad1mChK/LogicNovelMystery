6763bd8a8416e1950a4bc15a16cfa2598e1a21d7
KruglovEgor <367327@edu.itmo.ru>
2024-11-06 19:58:41 +0300

    fix
    Added email instead of confirming password

--- lnm-frontend/src/pages/Register.tsx
+++ lnm-frontend/src/pages/Register.tsx
@@ -2,14 +2,11 @@ import React, { useState } from 'react';
 
 const Register: React.FC = () => {
     const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
 
     const handleRegister = () => {
-        if (password !== confirmPassword) {
-            alert('Passwords do not match!');
-            return;
-        }
+
         // Логика для обработки регистрации
         console.log('Registration attempt:', { username, password });
     };
@@ -25,18 +22,18 @@ const Register: React.FC = () => {
                 onChange={(e) => setUsername(e.target.value)}
             />
             <input
+                type="text"
+                className="input-field" /* Добавляем класс */
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
                 type="password"
                 className="input-field" /* Добавляем класс */
                 placeholder="Password"
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
-            />
-            <input
-                type="password"
-                className="input-field" /* Добавляем класс */
-                placeholder="Confirm Password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
             />
             <button onClick={handleRegister}>Register</button>
         </div>
