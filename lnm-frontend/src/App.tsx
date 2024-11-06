9471cdad76b1982b7c55caf9bcd5cfa914b98540
KruglovEgor <367327@edu.itmo.ru>
2024-11-06 20:30:22 +0300

    new mock
    Added new mock page 'dashboard' for authorized users

--- lnm-frontend/src/App.tsx
+++ lnm-frontend/src/App.tsx
@@ -3,13 +3,16 @@ import {Routes, Route, Navigate} from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import './App.css';
+import Dashboard from "./pages/Dashboard.tsx";
 
 const App: React.FC = () => {
     return (
         <Routes>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="auth/login" element={<Login />} />
+            <Route path="auth/register" element={<Register />} />
+            <Route path="dashboard" element={<Dashboard />} />
+
+            <Route path="/" element={<Navigate to="auth/login" />} />
         </Routes>
     );
 };
