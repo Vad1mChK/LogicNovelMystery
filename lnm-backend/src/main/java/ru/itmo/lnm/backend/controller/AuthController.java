c044eaf251bcde924410bc30e379973640bfeb57
user1 <slava.css14@bk.ru>
2024-11-12 19:04:19 +0300

    add compiler force

---
+++ lnm-backend/src/main/java/ru/itmo/lnm/backend/controller/AuthController.java
@@ -0,0 +1,45 @@
+package ru.itmo.lnm.backend.controller;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.itmo.lnm.backend.dto.JwtDto;
+import ru.itmo.lnm.backend.dto.RegisterUserDto;
+import ru.itmo.lnm.backend.messages.JwtResponse;
+import ru.itmo.lnm.backend.service.AuthService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody @Valid RegisterUserDto registerUserDto) {
+        try {
+            authService.registerUser(registerUserDto);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body("Registration failed: " + e.getMessage());
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> login(@RequestBody @Valid JwtDto jwtDto) {
+        JwtResponse jwtResponse = authService.authenticateUser(jwtDto);
+        if (jwtResponse != null) {
+            return ResponseEntity.ok(jwtResponse);
+        } else {
+            return ResponseEntity.status(401).body(null);
+        }
+    }
+}
