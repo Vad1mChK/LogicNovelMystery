00a9c1c308c699f694ee232a9d51c20aebb9b20c
Vadim Chaykin <snusmumrike2@gmail.com>
2024-11-13 18:50:56 +0300

    Update App.tsx

--- lnm-frontend/src/App.tsx
+++ lnm-frontend/src/App.tsx
@@ -12,12 +12,12 @@ import Dashboard from './pages/Dashboard.tsx';
 const App: React.FC = () => {
 	return (
 		<Routes>
-			<Route path="auth/login" element={<Login />} />
-			<Route path="auth/register" element={<Register />} />
-			<Route path="dashboard" element={<Dashboard />} />
+			<Route path="/" element={<Navigate to="/auth/login" />} />
+			<Route path="/auth/login" element={<Login />} />
+			<Route path="/auth/register" element={<Register />} />
+			<Route path="/dashboard" element={<Dashboard />} />
 			{/* <Route path="/main" element={<MainPage />} /> */}
 			{/* <Route path="/select" element={<SelectedMode />} /> */}
-			<Route path="/" element={<Navigate to="auth/login" />} />
 		</Routes>
 	);
 };
