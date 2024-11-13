d897580b773326e1494cfb5720a202c38cba294c
KruglovEgor <367327@edu.itmo.ru>
2024-11-13 22:08:47 +0300

    replaced mock
    Now main page instead of dashboard

--- lnm-frontend/src/App.tsx
+++ lnm-frontend/src/App.tsx
@@ -4,18 +4,16 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import './App.css';
 // import './css/App.css';
-// import MainPage from './pages/MainPage';
+import MainPage from './pages/MainPage';
 // import SelectedMode from './pages/SelectedMode';
 // import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './pages/Dashboard.tsx';
 
 const App: React.FC = () => {
 	return (
 		<Routes>
 			<Route path="auth/login" element={<Login />} />
 			<Route path="auth/register" element={<Register />} />
-			<Route path="dashboard" element={<Dashboard />} />
-			{/* <Route path="/main" element={<MainPage />} /> */}
+			<Route path="/main" element={<MainPage />} />
 			{/* <Route path="/select" element={<SelectedMode />} /> */}
 			<Route path="/" element={<Navigate to="auth/login" />} />
 		</Routes>
