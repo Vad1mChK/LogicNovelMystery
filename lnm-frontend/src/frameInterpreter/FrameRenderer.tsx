91d82bf1fad86b113f266d0a00ae5d197173410f
Vadim Chaykin <snusmumrike2@gmail.com>
2024-11-28 21:59:56 +0300

    tried to implement game over logic

--- lnm-frontend/src/frameInterpreter/FrameRenderer.tsx
+++ lnm-frontend/src/frameInterpreter/FrameRenderer.tsx
@@ -6,6 +6,8 @@ import CharacterSprite from './CharacterSprite';
 import LocationBackground from './LocationBackground';
 // import '../css/FrameInterpreter.scss';
 import KnowledgeWindow from './KnowledgeWindow.tsx';
+import HealthBar from './HealthBar.tsx';
+import { useGameState } from './GameStateContext.tsx';
 
 interface FrameRendererProps {
 	frame: LnmFrame;
@@ -15,6 +17,7 @@ interface FrameRendererProps {
 	currentLocation: LnmLocation | null;
 	knowledge: string[];
 	onNextFrame: (nextFrameId: string) => void;
+	onGiveUp: () => void;
 }
 
 const FrameRenderer: React.FC<FrameRendererProps> = ({
@@ -25,11 +28,13 @@ const FrameRenderer: React.FC<FrameRendererProps> = ({
 	currentLocation,
 	knowledge,
 	onNextFrame,
+	onGiveUp,
 }) => {
 	const handleChoiceSelect = (nextFrameId: string) => {
 		onNextFrame(nextFrameId);
 	};
 	const [isKnowledgeOpen, setKnowledgeOpen] = useState(false);
+	const { health } = useGameState();
 
 	const knowledgeDetails = knowledge
 		.map((id) => plot.knowledge.get(id))
@@ -79,18 +84,29 @@ const FrameRenderer: React.FC<FrameRendererProps> = ({
 					-&gt;
 				</button>
 			)}
-			<button
-				className="view-knowledge-button game-button"
-				onClick={() => setKnowledgeOpen(true)}
-			>
-				Знания
-			</button>
+			<div className="top-button-bar">
+				<button
+					className="game-button"
+					onClick={() => setKnowledgeOpen(true)}
+				>
+					Знания
+				</button>
+				<button
+					className="game-button give-up-button"
+					onClick={onGiveUp}
+				>
+					Сдаться
+				</button>
+			</div>
+
 			{isKnowledgeOpen && (
 				<KnowledgeWindow
 					knowledge={knowledgeDetails}
 					onClose={() => setKnowledgeOpen(false)}
 				/>
 			)}
+
+			<HealthBar currentHealth={health} maxHealth={100} />
 		</div>
 	);
 };
