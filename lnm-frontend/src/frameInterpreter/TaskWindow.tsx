1db5468109f8b72cece9998069e1036e4f4489b9
Vadim Chaykin <snusmumrike2@gmail.com>
2024-12-11 23:07:18 +0300

    use material-ui for tasks

--- lnm-frontend/src/frameInterpreter/TaskWindow.tsx
+++ lnm-frontend/src/frameInterpreter/TaskWindow.tsx
@@ -4,10 +4,14 @@ import { LnmTask, LnmTaskType } from './types.ts';
 import SyntaxHighlightInput from '../markupElements/SyntaxHighlightInput.tsx';
 import TextSyntaxHighlighter from '../markupElements/TextSyntaxHighlighter.tsx';
 import HintDisplay from "../markupElements/HintDisplay.tsx";
+import { Checkbox, colors, FormControlLabel, FormGroup, Radio, RadioGroup } from "@mui/material";
+import '../css/TaskWindow.scss';
 
 interface TaskWindowProps {
 	task: LnmTask,
 }
+
+const _WHITE = '#F4F4F4';
 
 const TaskWindow: React.FC<TaskWindowProps> = ({
 	task
@@ -18,47 +22,53 @@ const TaskWindow: React.FC<TaskWindowProps> = ({
 				width: '50vw',
 				height: '50vh',
 			}}
+			className="task-window"
 		>
-			<TextSyntaxHighlighter input={task.questionText} />
+			<TextSyntaxHighlighter input={task.questionText} copyable />
 			{task.hint && <HintDisplay hintText={task.hint} />}
-			{task.type == LnmTaskType.SELECT_MANY && (
-				<ul>
+			<hr />
+			{task.type === LnmTaskType.SELECT_MANY && (
+				<FormGroup>
 					{task.options?.map((option, index) => (
-						<li key={index}>
-							<input
-								type="checkbox"
-								name="select-many"
-								id={`select-many-${index}`}
-								value={index}
-							/>
-							<label htmlFor={`select-many-${index}`}>
-								{option}
-							</label>
-						</li>
-					)) ?? <li>Error loading answer options</li>}
-				</ul>
+						<FormControlLabel
+							key={index}
+							control={
+								<Checkbox name="select-many" value={index} sx={{color: _WHITE}} color='default'/>
+							}
+							label={<TextSyntaxHighlighter input={option} copyable={false}/>}
+						/>
+					)) ?? <div>Error loading answer options</div>}
+				</FormGroup>
 			)}
-			{task.type == LnmTaskType.SELECT_ONE && (
-				<ul>
+			{/**/}
+			{task.type === LnmTaskType.SELECT_ONE && (
+				<RadioGroup name="select-one">
 					{task.options?.map((option, index) => (
-						<li key={index}>
-							<input
-								type="radio"
-								name="select-many"
-								id={`select-many-${index}`}
-								defaultChecked={index === 0}
-								required={index === 0}
-							/>
-							<label htmlFor={`select-many-${index}`}>
-								{option}
-							</label>
-						</li>
-					)) ?? <li>Error loading answer options</li>}
-				</ul>
+						<FormControlLabel
+							key={index}
+							control={
+								<Radio
+									name="select-one"
+									value={index}
+									defaultChecked={index === 0}
+									required={index === 0}
+									sx={{
+										color: _WHITE,
+									}}
+									color='default'
+								/>
+							}
+							label={<TextSyntaxHighlighter input={option} copyable={false}/>}
+						/>
+					)) ?? <div>Error loading answer options</div>}
+				</RadioGroup>
 			)}
 			{(task.type == LnmTaskType.WRITE_KNOWLEDGE ||
 				task.type == LnmTaskType.COMPLETE_QUERY) && (
-				<SyntaxHighlightInput placeholder={task.default} />
+				<SyntaxHighlightInput
+					placeholder='Enter your solution here...'
+					value={task.default}
+				/>
 			)}
 			<input type="submit" value={'Отправить'} />
 		</form>
