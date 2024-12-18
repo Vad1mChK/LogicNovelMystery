f9479ab06b6cd3adcc148feeb99156ccdc858756
Vadim Chaykin <snusmumrike2@gmail.com>
2024-12-19 00:39:31 +0300

    moved store.ts

--- lnm-frontend/src/state/store.ts
+++ lnm-frontend/src/state/store.ts
@@ -1,16 +1,19 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gameStateReducer, {
 	GameState,
-} from './frameInterpreter/gameStore/gameStateSlice';
+} from './gameStateSlice.ts';
+import languageReducer, { LanguageState } from './languageSlice.ts';
 
 const store = configureStore({
 	reducer: {
 		gameState: gameStateReducer,
+		languageState: languageReducer,
 	},
 });
 
 export type RootState = {
 	gameState: GameState;
+	languageState: LanguageState;
 };
 export type AppDispatch = typeof store.dispatch;
 
