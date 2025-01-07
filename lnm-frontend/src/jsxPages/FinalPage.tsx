491608187d4bf9257c1c32222708f77a1e274701
Vadim Chaykin <snusmumrike2@gmail.com>
2025-01-07 18:27:34 +0300

    formatter

--- lnm-frontend/src/jsxPages/FinalPage.tsx
+++ lnm-frontend/src/jsxPages/FinalPage.tsx
@@ -3,24 +3,24 @@ import { useNavigate } from 'react-router-dom';
 import '../css/FinalPage.scss';
 
 const FinalPage: React.FC = () => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    return (
-        <div className="background">
-            <div className="overlay">
-                <div className="content-container">
-                    <h1>Игра завершена</h1>
-                    <p>Ваш счёт: </p>
-                    <button
-                        className="return-button"
-                        onClick={() => navigate('/main')}
-                    >
-                        Вернуться на главную
-                    </button>
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="background">
+			<div className="overlay">
+				<div className="content-container">
+					<h1>Игра завершена</h1>
+					<p>Ваш счёт: </p>
+					<button
+						className="return-button"
+						onClick={() => navigate('/main')}
+					>
+						Вернуться на главную
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default FinalPage;
