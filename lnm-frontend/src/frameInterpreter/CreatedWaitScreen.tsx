d581bc9bba214c0be452ee4b22969719a67e4062
Vadim Chaykin <snusmumrike2@gmail.com>
2025-01-14 00:57:22 +0300

    fixed paths

--- lnm-frontend/src/frameInterpreter/CreatedWaitScreen.tsx
+++ lnm-frontend/src/frameInterpreter/CreatedWaitScreen.tsx
@@ -4,26 +4,32 @@ import mansionEntrance from '../assets/img/locations/MansionEntrance.webp';
 import '../css/CreateOrResultsScreen.scss';
 import { BASE_URL } from '../metaEnv';
 import CharacterSprite from './CharacterSprite';
-import { LnmCharacter, LnmFrameCharacterData } from './types';
+import { LnmCharacter, LnmFrameCharacterData, LnmHero } from './types';
 
 interface CreatedWaitScreenProps {
-	protagonist: string;
+	protagonist: LnmHero;
 }
 
 const CreatedWaitScreen: React.FC<CreatedWaitScreenProps> = ({
 	protagonist,
 }) => {
-	const protagonistToLocationImage: Map<string, string> = new Map([
-		['steve', `${BASE_URL}assets/img/locations/AgentOffice.webp`],
-		['professor', `${BASE_URL}assets/img/locations/ProfessorBedroom.webp`],
-		['vicky', `${BASE_URL}assets/img/locations/VickyRoom.webp`],
+	const protagonistToLocationImage: Map<LnmHero, string> = new Map([
+		[LnmHero.STEVE, `${BASE_URL}assets/img/locations/AgentOffice.webp`],
+		[
+			LnmHero.PROFESSOR,
+			`${BASE_URL}assets/img/locations/ProfessorBedroom.webp`,
+		],
+		[LnmHero.VICKY, `${BASE_URL}assets/img/locations/VickyRoom.webp`],
 	]);
 	const protagonistCharacter: LnmCharacter = {
 		id: protagonist,
-		name: t(`game.protagonist.${protagonist}`),
+		name: t(`game.protagonist.${protagonist.toLowerCase()}`),
 		defaultPose: 'idle',
 		sprites: new Map([
-			['idle', `assets/img/characters/${protagonist}/idle.webp`],
+			[
+				'idle',
+				`assets/img/characters/${protagonist.toLowerCase()}/idle.webp`,
+			],
 		]),
 	};
 	const protagonistCharacterData: LnmFrameCharacterData = {
@@ -46,7 +52,9 @@ const CreatedWaitScreen: React.FC<CreatedWaitScreenProps> = ({
 				<p>{t('game.createdWaitScreen.wait')}</p>
 				<p>
 					{t('game.createdWaitScreen.willPlayAs', {
-						protagonist: t(`game.protagonist.${protagonist}`),
+						protagonist: t(
+							`game.protagonist.${protagonist.toLowerCase()}`
+						),
 					})}
 				</p>
 			</div>
