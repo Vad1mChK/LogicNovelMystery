b98ab413cf3717879fca2065d6d437a3052e1be4
Vadim Chaykin <snusmumrike2@gmail.com>
2025-01-16 02:13:11 +0300

    restoreState.ts

---
+++ lnm-frontend/src/frameInterpreter/communication/restoreState.ts
@@ -0,0 +1,48 @@
+// restoreState.ts
+import axios from 'axios';
+import { VITE_SERVER_URL } from '../../metaEnv';
+import {
+	increaseHealth,
+	setCurrentChapter,
+	setHealth,
+} from '../../state/gameStateSlice';
+import { AppDispatch } from '../../state/store'; // or wherever your store types live
+
+export const restoreState = async (
+	sessionToken: string,
+	isMultiplayer: boolean,
+	dispatch: AppDispatch
+): Promise<boolean> => {
+	try {
+		const response = await axios.post(
+			`${VITE_SERVER_URL}/restore-state`,
+			{ sessionToken, isMultiplayer },
+			{ headers: { Authorization: localStorage.getItem('AuthToken') } }
+		);
+		if (!response.data) {
+			console.warn('No state data received from server');
+			return true;
+		}
+		const health = response.data?.userHp;
+		if (health !== null && health !== undefined) {
+			console.log('Restoring health to:', health);
+			dispatch(setHealth(health));
+		} else {
+			console.warn('No health data received from server');
+			dispatch(increaseHealth('full'));
+		}
+
+		const chapter = response.data?.chapter;
+		if (chapter !== null && chapter !== undefined) {
+			console.log('Restoring chapter to:', chapter);
+			dispatch(setCurrentChapter(chapter));
+		} else {
+			console.warn('No chapter data received from server');
+			dispatch(setCurrentChapter('inception1'));
+		}
+		return true;
+	} catch (error) {
+		console.error('Error restoring the game state from server:', error);
+		return false;
+	}
+};
